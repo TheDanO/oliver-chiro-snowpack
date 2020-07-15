9bb10c9870056072bcf88e150254ee0113b50701
Daniel Oliver <danoliver@bc.com>
2020-07-14 21:47:12 -0600

    Initial snowpack / 11ty / tailwind commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.njk',
+    './_includes/**/*.html',
+    './_includes/**/*.njk',
+    './_template/**/*.html',
+    './_template/**/*.njk',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
